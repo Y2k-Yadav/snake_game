d5bb0eaad9ca82938858a8ab458aa1fca5145d48
username1-cyber <63380531+username1-cyber@users.noreply.github.com>
2023-12-23 21:04:51 +0530

    Update main.py
    where we are using scoreboard function in which we are updating the score using the increasing _score and update_score method using Scoreboard Class and we are importing the Scoreboard and creating object in main.py and we are using the concept called inheritance in Scoreboard class.....

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from food import Food
 from snake import Snake
+from scoreboard import Scoreboard
 import time
 
 turtle = Turtle()
@@ -11,6 +12,7 @@ screen.title("SNAKE GAME")
 screen.tracer(0)
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -18,11 +20,14 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 game_is_on = True
 # move snake
+new = 0
 while game_is_on:
 
     screen.update()
     time.sleep(0.1)
     snake.move_snake()
-    if snake.head.distance(food)<15:
+    if snake.head.distance(food) < 20:
         food.refresh()
+        scoreboard.increase_score()
 screen.exitonclick()
+
