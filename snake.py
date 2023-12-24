cc0ec25ee7ec186a7fae8272027cc10d09f46d25
username1-cyber <63380531+username1-cyber@users.noreply.github.com>
2023-12-24 08:42:55 +0530

    Update snake.py
    where we creating a new two more class where we are add_segment(position) where it position arugment from new fucntion called extend _new_tail() in which we are passing the position of last segment coordinates and in which we are getting new tail

--- snake.py
+++ snake.py
@@ -7,7 +7,9 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
+
     def __init__(self):
         self.segments = []
         self.create_snake()
@@ -15,12 +17,16 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+    def extend_new_tail(self):
+        self.add_segment(self.segments[-1].position())
 
     def move_snake(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -31,15 +37,17 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-       if  self.head.heading() != DOWN:
+        if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
+
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
     def down(self):
         if self.head.heading() != UP:
-         self.head.setheading(DOWN)
+            self.head.setheading(DOWN)
